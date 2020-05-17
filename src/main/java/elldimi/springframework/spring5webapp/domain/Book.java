78a8457efbec988321250da59ca8e857ce286717
EleonoraDM <elinor.dm@gmail.com>
2020-05-17 14:42:43 +0300

    files uploading

---
+++ src/main/java/elldimi/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,42 @@
+package elldimi.springframework.spring5webapp.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
